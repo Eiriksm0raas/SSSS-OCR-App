1cda258775f520619d5f439aba8169f33c43066b
ihalvor <halvoras97@hotmail.com>
2023-05-24 13:57:44 +0200

    Added project
    Start to the project with FastAPI.

---
+++ Project/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get('/')
+def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
